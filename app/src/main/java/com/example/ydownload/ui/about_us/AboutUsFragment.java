75ada417919ac9f571eeb1b9958f0f5f1db57f32
abubasith456 <abubasith456@gmail.com>
2022-04-09 05:24:14 +0530

    Download error fixed

--- app/src/main/java/com/example/ydownload/ui/about_us/AboutUsFragment.java
+++ app/src/main/java/com/example/ydownload/ui/about_us/AboutUsFragment.java
@@ -23,8 +23,6 @@ public class AboutUsFragment extends Fragment {
 
         binding = FragmentAboutUsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        final TextView textView = binding.textGallery;
-        aboutUsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
